5b9567542d2fe10e7790ff0bf3ff715786f103d6
lmagnette <loicmagnette@gmail.com>
2023-10-25 23:40:41 +0200

    setup project

---
+++ dinosaur/src/test/java/be/lomagnette/dinosaur/DinosaurApplicationTests.java
@@ -0,0 +1,13 @@
+package be.lomagnette.dinosaur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinosaurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
