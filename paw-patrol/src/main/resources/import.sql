91c1df0d191818e70319e0988988cd3f3527eff3
lmagnette <loicmagnette@gmail.com>
2023-10-26 00:31:35 +0200

    Add data import script for 'paw' table
    Introduced 'import.sql' that includes data for the 'paw' table. This SQL file deletes all entries from the 'paw' table and subsequently inserts a set of predefined entries. This will be particularly useful in development and testing environments, allowing for consistent and relevant data to be imported and worked with.

---
+++ paw-patrol/src/main/resources/import.sql
@@ -0,0 +1,24 @@
+DELETE
+FROM paw;
+INSERT INTO paw (id, name, description, fun_fact)
+VALUES (1001, 'Ryder',
+        'Un garçon de dix ans qui est le leader de la PAW Patrol. Il est très organisé et a toujours un plan.',
+        'Ryder est très bon pour réparer les choses, surtout les véhicules.'),
+       (1002, 'Chase', 'Un berger allemand qui sert de chien policier et espion. Il est mature, sérieux et athlétique.',
+        'Chase est allergique aux chats et aux plumes.'),
+       (1003, 'Marcus',
+        'Un dalmatien qui sert de pompier et de médecin. Il est maladroit mais vient toujours à la rescousse quand on a besoin de lui.',
+        'Marshall a tendance à mettre de leau partout quand il est excité.'),
+       (1004, 'Stella', 'Un Cockapoo qui sert d''aviatrice. Elle est courageuse et aime voler dans son hélicoptère.',
+        'Skye adore faire des flips arrière dans l air pendant les missions.'),
+       (1005, 'Ruben', 'Un Bulldog qui sert de chien de construction. Il est fort et passionné de skateboarding.',
+        'Rubble était un chiot errant avant de rejoindre la PAW Patrol.'),
+       (1006, 'Rocky',
+        'Un chiot de race mixte qui sert de chien recycleur et de bricoleur. Il déteste l eau mais adore réparer les choses.',
+        'Rocky peut généralement trouver une solution en utilisant de vieilles pièces de rechange.'),
+       (1007, 'Zuma',
+        'Un labrador retriever qui sert de chien de sauvetage aquatique. Il est enthousiaste et aime surfer.',
+        'Zuma a un léger défaut de prononciation où il ne peut pas prononcer correctement ses "r".'),
+       (1008, 'Everest',
+        'Une Husky qui sert de chien de sauvetage dans la neige. Elle adore la neige et est toujours prête pour l action.',
+        'Everest aime manger quelque chose avec un goût de foie.');
